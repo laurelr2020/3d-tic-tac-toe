dc843cc0bd4cf2fcfe1131e9fb3ffbc36672a91c
laurelr2020 <reganlaurell@gmail.com>
2019-10-30 10:58:13 -0400

    set up tic tac toe board

---
+++ tic-tac-toe.js
@@ -0,0 +1,32 @@
+function drawTicTacToeBoard()
+{
+	// glBegin(GL_LINES);
+    glColor3f(255, 255, 255);
+    glBegin(GL_POLYGON);
+        glVertex3f(20, 45, cubeVertexZ[2] + 1);
+        glVertex3f(24, 45, cubeVertexZ[3] + 1);
+        glVertex3f(20, -45, cubeVertexZ[7] + 1);
+        glVertex3f(24, -45, cubeVertexZ[6] + 1);
+    glEnd();
+
+    glBegin(GL_POLYGON);
+        glVertex3f(-20, 45, cubeVertexZ[2] + 1);
+        glVertex3f(-24, 45, cubeVertexZ[3] + 1);
+        glVertex3f(-20, -45, cubeVertexZ[7] + 1);
+        glVertex3f(-24, -45, cubeVertexZ[6] + 1);
+    glEnd();
+    
+    glBegin(GL_POLYGON);
+        glVertex3f(45,  18, cubeVertexZ[2] + 1);
+        glVertex3f(45,  22, cubeVertexZ[3] + 1);
+        glVertex3f(-45, 18, cubeVertexZ[7] + 1);
+        glVertex3f(-45, 22, cubeVertexZ[6] + 1);
+    glEnd();
+
+    glBegin(GL_POLYGON);
+        glVertex3f( 45, -18, cubeVertexZ[2] + 1);
+        glVertex3f( 45, -22, cubeVertexZ[3] + 1);
+        glVertex3f(-45, -18, cubeVertexZ[7] + 1);
+        glVertex3f(-45, -22, cubeVertexZ[6] + 1);
+    glEnd();
+}
