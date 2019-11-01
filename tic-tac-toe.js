c03b4cfcbd0cf6ee83dc9b6b0d4664dbcd65fc7b
laurelr2020 <reganlaurell@gmail.com>
2019-11-01 14:18:06 -0400

    create O game piece

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,55 +1,65 @@
 function drawTicTacToeBoard(){
-    glColor3f(255, 255, 255);
+    glColor3f(1.0, 1.0, 1.0);
     glBegin(GL_POLYGON);
-        glVertex3f(20, 45, cubeVertexZ[2] + 1);
-        glVertex3f(24, 45, cubeVertexZ[3] + 1);
-        glVertex3f(20, -45, cubeVertexZ[7] + 1);
-        glVertex3f(24, -45, cubeVertexZ[6] + 1);
+        glVertex3f(20,  45,  postiveZ + 1);
+        glVertex3f(24,  45,  postiveZ + 1);
+        glVertex3f(20, -45, postiveZ + 1);
+        glVertex3f(24, -45, postiveZ + 1);
     glEnd();
 
     glBegin(GL_POLYGON);
-        glVertex3f(-20,  45, cubeVertexZ[2] + 1);
-        glVertex3f(-24,  45, cubeVertexZ[3] + 1);
-        glVertex3f(-20, -45, cubeVertexZ[7] + 1);
-        glVertex3f(-24, -45, cubeVertexZ[6] + 1);
+        glVertex3f(-20,  45, postiveZ + 1);
+        glVertex3f(-24,  45, postiveZ + 1);
+        glVertex3f(-20, -45, postiveZ + 1);
+        glVertex3f(-24, -45, postiveZ + 1);
     glEnd();
     
     glBegin(GL_POLYGON);
-        glVertex3f(45,  18, cubeVertexZ[2] + 1);
-        glVertex3f(45,  22, cubeVertexZ[3] + 1);
-        glVertex3f(-45, 18, cubeVertexZ[7] + 1);
-        glVertex3f(-45, 22, cubeVertexZ[6] + 1);
+        glVertex3f(45,  18, postiveZ + 1);
+        glVertex3f(45,  22, postiveZ + 1);
+        glVertex3f(-45, 18, postiveZ + 1);
+        glVertex3f(-45, 22, postiveZ + 1);
     glEnd();
 
     glBegin(GL_POLYGON);
-        glVertex3f( 45, -18, cubeVertexZ[2] + 1);
-        glVertex3f( 45, -22, cubeVertexZ[3] + 1);
-        glVertex3f(-45, -18, cubeVertexZ[7] + 1);
-        glVertex3f(-45, -22, cubeVertexZ[6] + 1);
+        glVertex3f( 45, -18, postiveZ + 1);
+        glVertex3f( 45, -22, postiveZ + 1);
+        glVertex3f(-45, -18, postiveZ + 1);
+        glVertex3f(-45, -22, postiveZ + 1);
     glEnd();
 }
 
 function drawXGamePiece(){
     glBegin(GL_POLYGON);
-        glVertex3f( -10,   8, cubeVertexZ[2] + 1);
-        glVertex3f( -10,  12, cubeVertexZ[3] + 1);
-        glVertex3f(  10,  -8, cubeVertexZ[7] + 1);
-        glVertex3f(  10, -12, cubeVertexZ[6] + 1);
+        glVertex3f( -10,   8, postiveZ + 1);
+        glVertex3f( -10,  12, postiveZ + 1);
+        glVertex3f(  10,  -8, postiveZ + 1);
+        glVertex3f(  10, -12, postiveZ + 1);
     glEnd();
 
     glBegin(GL_POLYGON);
-        glVertex3f(  10,  8, cubeVertexZ[7] + 1);
-        glVertex3f(  10, 12, cubeVertexZ[6] + 1);
-        glVertex3f( -10, -8, cubeVertexZ[2] + 1);
-        glVertex3f( -10,-12, cubeVertexZ[3] + 1);
+        glVertex3f(  10,  8, postiveZ + 1);
+        glVertex3f(  10, 12, postiveZ + 1);
+        glVertex3f( -10, -8, postiveZ + 1);
+        glVertex3f( -10,-12, postiveZ + 1);
     glEnd();
 }
 
 function drawOGamePiece(){
-    glBegin(GL_LINE_LOOP);
-        glVertex2f(  10,  8);
-        glVertex2f(  10, 12);
-        glVertex2f( -10, -8);
-        glVertex2f( -10,-12);
+    glColor3f(1.0, 1.0, 1.0);
+    fillCircle(15, postiveZ + 1);
+
+    glColor3f(0.0, 1.0, 0.0);
+    fillCircle(10, postiveZ + 2);
+}
+
+function fillCircle(radius, zIndex){
+    glBegin(GL_POLYGON);
+    for (let thetaInDegrees = 0; thetaInDegrees < 360; thetaInDegrees++) {
+        var theta = thetaInDegrees * Math.PI / 180;
+        var x = radius * Math.cos(theta);
+        var y = radius * Math.sin(theta);
+        glVertex3f(x, y, zIndex);
+    } 
     glEnd();
 }
