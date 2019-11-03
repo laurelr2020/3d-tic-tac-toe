a209c0edfc6a4c042b7bb857a837ee0944b6fb33
laurelr2020 <reganlaurell@gmail.com>
2019-11-03 18:22:00 -0500

    move tic tac toe game board methods to game-pieces file

--- game-pieces.js
+++ game-pieces.js
@@ -1,5 +1,4 @@
 /** Game Pieces */
-
 /**
  * Tic-Tac-Toe X
  */
@@ -43,3 +42,73 @@ const rightMiddleSquareO = [ 34, 0];
 const leftBottomSquareO   = [-34, -35];
 const middleBottomSquareO = [  0, -34];
 const rightBottomSquareO  = [ 34, -35];
+
+/**draws tic-tac-toe-board on Green Side of cube */
+function drawTicTacToeBoard(){
+    glColor3f(1.0, 1.0, 1.0);
+    glBegin(GL_POLYGON);
+        glVertex3f(18,  45,  postiveZ + 1);
+        glVertex3f(22,  45,  postiveZ + 1);
+        glVertex3f(18, -45, postiveZ + 1);
+        glVertex3f(22, -45, postiveZ + 1);
+    glEnd();
+
+    glBegin(GL_POLYGON);
+        glVertex3f(-18,  45, postiveZ + 1);
+        glVertex3f(-22,  45, postiveZ + 1);
+        glVertex3f(-18, -45, postiveZ + 1);
+        glVertex3f(-22, -45, postiveZ + 1);
+    glEnd();
+    
+    glBegin(GL_POLYGON);
+        glVertex3f( 45, 18, postiveZ + 1);
+        glVertex3f( 45, 22, postiveZ + 1);
+        glVertex3f(-45, 18, postiveZ + 1);
+        glVertex3f(-45, 22, postiveZ + 1);
+    glEnd();
+
+    glBegin(GL_POLYGON);
+        glVertex3f( 45, -18, postiveZ + 1);
+        glVertex3f( 45, -22, postiveZ + 1);
+        glVertex3f(-45, -18, postiveZ + 1);
+        glVertex3f(-45, -22, postiveZ + 1);
+    glEnd();
+}
+
+/**draws the X game piece in the square that is passed in as a parameter */
+function drawXGamePiece(gamePiecePlace){
+    glBegin(GL_POLYGON);
+        glVertex3f( gamePiecePlace[0], gamePiecePlace[1], postiveZ + 1);
+        glVertex3f( gamePiecePlace[2], gamePiecePlace[3], postiveZ + 1);
+        glVertex3f( gamePiecePlace[4], gamePiecePlace[5], postiveZ + 1);
+        glVertex3f( gamePiecePlace[6], gamePiecePlace[7], postiveZ + 1);
+    glEnd();
+
+    glBegin(GL_POLYGON);
+        glVertex3f( gamePiecePlace[8],  gamePiecePlace[9],  postiveZ + 1);
+        glVertex3f( gamePiecePlace[10], gamePiecePlace[11], postiveZ + 1);
+        glVertex3f( gamePiecePlace[12], gamePiecePlace[13], postiveZ + 1);
+        glVertex3f( gamePiecePlace[14], gamePiecePlace[15], postiveZ + 1);
+    glEnd();
+}
+
+/**draws the O game piece in the square that is passed in as a parameter */
+function drawOGamePiece(gamePieceCenter){
+    glColor3f(1.0, 1.0, 1.0);
+    fillCircle(outerRadius, gamePieceCenter[0], gamePieceCenter[1], postiveZ + 1);
+
+    glColor3f(0.0, 1.0, 0.0);
+    fillCircle(innerRadius, gamePieceCenter[0], gamePieceCenter[1], postiveZ + 2);
+}
+
+/**fills a circle for the O game piece based on the radius and center of circle that is passed in */
+function fillCircle(radius, centerX, centerY, zIndex){
+    glBegin(GL_POLYGON);
+    for (let thetaInDegrees = 0; thetaInDegrees < 360; thetaInDegrees++) {
+        var theta = thetaInDegrees * Math.PI / 180;
+        var x = radius * Math.cos(theta) + centerX;
+        var y = radius * Math.sin(theta) + centerY;
+        glVertex3f(x, y, zIndex);
+    } 
+    glEnd();
+}
