0f5aa3d3aa57b16f9ea6159822cd16cb9afe7183
laurelr2020 <reganlaurell@gmail.com>
2019-11-03 18:14:57 -0500

    add winning conditions

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -63,3 +63,15 @@ function fillCircle(radius, centerX, centerY, zIndex){
     } 
     glEnd();
 }
+
+var winningConditions = new Array();
+winningConditions.push([1, 2, 3]);
+winningConditions.push([4, 5, 6]);
+winningConditions.push([7, 8, 9]);
+
+winningConditions.push([1, 4, 7]);
+winningConditions.push([2, 5, 8]);
+winningConditions.push([3, 6, 9]);
+
+winningConditions.push([1, 5, 9]);
+winningConditions.push([3, 5, 7]);
