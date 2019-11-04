0218987164612cd32d0b4d9ba97b2f1699c2f75c
laurelr2020 <reganlaurell@gmail.com>
2019-11-04 13:27:24 -0500

    add in initial code for mouse events

--- open-gl.js
+++ open-gl.js
@@ -87,17 +87,25 @@ function doKeyUp(evt) {
 // --------------- support for mouse events --------------------------------------
 
 /**
-* This function is called in init() to set up mouse event handling
-* on the canvas.  You can modify the nested functions doMouseDown,
-* doMouseDrag, and possibly doMouseUp to change the reponse to
-* mouse events.  See the lines labeled "TODO".
-*/
+ * This function is called in init() to set up mouse event handling
+ * on the canvas.  You can modify the nested functions doMouseDown,
+ * doMouseDrag, and possibly doMouseUp to change the reponse to
+ * mouse events.  See the lines labeled "TODO".
+ */
 function installMouseHandler(canvasID) {
+
     var canvas = document.getElementById(canvasID);
+
+    var dragging = false;  // set to true when a drag action is in progress.
+    var startX, startY;    // coordinates of mouse at start of drag.
+    var prevX, prevY;      // previous mouse position during a drag.
     
     function doMouseDown(evt) {
             // This function is called when the user presses a button on the mouse.
             // Only the main mouse button will start a drag.
+        if (dragging) {
+            return;  // if a drag is in progress, don't start another.
+        }
         if (evt.button != 0) {
             return;  // don't respond unless the button is the main (left) mouse button.
         }
@@ -105,52 +113,58 @@ function installMouseHandler(canvasID) {
         var r = canvas.getBoundingClientRect();
         x = Math.round(evt.clientX - r.left);  // translate mouse position from screen coords to canvas coords.
         y = Math.round(evt.clientY - r.top);   // round to integer values; some browsers would give non-integers.
-        // if (dragging) {
-        //     startX = prevX = x;
-        //     startY = prevY = y;
-        //     document.addEventListener("mousemove", doMouseMove, false);
-        //     document.addEventListener("mouseup", doMouseUp, false);
-        // }     
-        console.log("end of mouse down");
+        dragging = true;  // (TODO: This might not be the case for all mousedowns in all programs.)
+        if (dragging) {
+            startX = prevX = x;
+            startY = prevY = y;
+            document.addEventListener("mousemove", doMouseMove, false);
+            document.addEventListener("mouseup", doMouseUp, false);
+        }        
     }
     
-    // function doMouseMove(evt) {
-    //         // This function is called when the user moves the mouse during a drag.
-    //     if (!dragging) {
-    //         return;  // (shouldn't be possible)
-    //     }
-    //     var x,y;  // mouse position in canvas coordinates
-    //     var r = canvas.getBoundingClientRect();
-    //     x = Math.round(evt.clientX - r.left);  //mouse position, in canvas coords, with (0,0) at upper left.
-    //     y = Math.round(evt.clientY - r.top);
+    function doMouseMove(evt) {
+            // This function is called when the user moves the mouse during a drag.
+        if (!dragging) {
+            return;  // (shouldn't be possible)
+        }
+        var x,y;  // mouse position in canvas coordinates
+        var r = canvas.getBoundingClientRect();
+        x = Math.round(evt.clientX - r.left);  //mouse position, in canvas coords, with (0,0) at upper left.
+        y = Math.round(evt.clientY - r.top);
 
-    //     // TODO:  ADD CODE TO REPSOND TO THE MOUSE BEING DRAGGED.
-    //     prevX = x;  // update prevX,prevY to prepare for next call to doMouseMove
-    //     prevY = y;
-    //     display();   // Redraw the image, to reflect the changes.
-    // }
+        // TODO:  ADD CODE TO REPSOND TO THE MOUSE BEING DRAGGED.
+
+        prevX = x;  // update prevX,prevY to prepare for next call to doMouseMove
+        prevY = y;
+        display();   // Redraw the image, to reflect the changes.
+    }
     
     function doMouseUp(evt) {
-
-        // document.removeEventListener("mousemove", doMouseMove, false);
-        // document.removeEventListener("mouseup", doMouseMove, false);
+            // This function is called when the user releases a mouse button during a drag.
+        if (!dragging) {
+            return;  // (shouldn't be possible)
+        }
+        dragging = false;
+        document.removeEventListener("mousemove", doMouseMove, false);
+        document.removeEventListener("mouseup", doMouseMove, false);
         // TODO:  Possibly, respond to mouse up and call repaint().
-        console.log("at end of MouseUp event");
     }
     
     canvas.addEventListener("mousedown", doMouseDown, false);
-}
+} 
 
 // --------------- support for animation ------------------------------------------
 
 /** You can call startAnimation() to run an animation.  A frame will be drawn every
-* 30 milliseconds (can be changed in the call to glutTimerFunc.  The global frameNumber
-* variable will be incremented for each frame.  Call pauseAnimation() to stop animating.
-*/
+ * 30 milliseconds (can be changed in the call to glutTimerFunc.  The global frameNumber
+ * variable will be incremented for each frame.  Call pauseAnimation() to stop animating.
+ */
 
-var animating = false;
+var animating = false;      // indicates whether an animation is in progress;
+                            // do not change directly; call startAnimation() and pauseAnimation()
 
 function updateFrame() {
+    // TODO: INSERT CODE TO UPDATE ANY OTHER DATA USED IN DRAWING A FRAME
     y += 0.3;
     if (y > 60) {
         y = -50;
@@ -167,6 +181,7 @@ function timerFunction() {
 }
 
 function startAnimation() {
+        // call this to start or restart the animation.
     if ( ! animating ) {
         animating = true;
         setTimeout(timerFunction, 30);
@@ -174,6 +189,7 @@ function startAnimation() {
 }
 
 function pauseAnimation() {
+            // call this to pause the animation.
     animating = 0;
 }
 // ------------------------- initialization functions -----------------------------------
